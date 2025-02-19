3841deffcff2c45bbf5d2d04730ab42891b7ffbb
FELIPE SANTOS PORTO <25132496@CFP127ALUNO.LOCAL>
2025-02-19 11:27:34 -0300

    Classe Paciente Iniciada

---
+++ src/br/senai/sp/jandira/clinica/Clinica.java
@@ -0,0 +1,31 @@
+package br.senai.sp.jandira.clinica;
+
+import br.senai.sp.jandira.clinica.model.Paciente;
+
+public class Clinica {
+
+	public static void main(String[] args) {
+
+		System.out.println("Eae");
+		
+
+		Paciente p1 = new Paciente();
+		p1.nome = "Felps";
+		p1.altura = 1.81;
+		p1.peso = 95;
+		p1.telefone = "119637375937";
+		p1.dataNascimento = LocalDate.of(2003, 7, 15);	
+		
+		Paciente p2 = new Paciente();
+		p2.nome = "Felipe";
+		p2.altura = 1.81;
+		p2.peso = 1..81;
+		p2.telefone = "119748988847"
+				
+
+		
+		
+		
+	}
+
+}
---
+++ src/br/senai/sp/jandira/clinica/model/Paciente.java
@@ -0,0 +1,29 @@
+package br.senai.sp.jandira.clinica.model;
+
+import java.time.LocalDate;
+
+public class Paciente {
+
+	String nome;
+	LocalDate dataNascimento;
+	int peso;
+	double altura;
+	String telefone;
+	
+	public void calcularIdade() {
+		
+	}
+	
+	public void calcularImc() {
+		
+	}
+	
+	public void mostrarFichaDoPaciente() {
+		
+	}
+	
+	
+	
+	
+	
+}
